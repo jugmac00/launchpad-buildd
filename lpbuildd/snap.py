32cff66a6b5a93c327dc64e7d0c8a9c17d570c4f
Kit Randel <kit.randel@canonical.com>
2015-10-06 09:38:11 +1300

    * Pass env to runSubProcess. * Expect proxy_url in build extra_args, rather than proxy_username, proxy_password, proxy_host and proxy_port.

--- lpbuildd/snap.py
+++ lpbuildd/snap.py
@@ -40,12 +40,9 @@ class SnapBuildManager(DebianBuildManager):
 
         self.name = extra_args["name"]
         self.branch = extra_args.get("branch")
-        self.build_cookie = extra_args.get("build_cookie")
         self.git_repository = extra_args.get("git_repository")
         self.git_path = extra_args.get("git_path")
-        self.proxy_host = extra_args.get("proxy_host")
-        self.proxy_port = extra_args.get("proxy_port")
-        self.proxy_token = extra_args.get("proxy_token")
+        self.proxy_url = extra_args.get("proxy_url")
 
         super(SnapBuildManager, self).initiate(files, chroot, extra_args)
 
@@ -56,15 +53,10 @@ class SnapBuildManager(DebianBuildManager):
             "--build-id", self._buildid,
             "--arch", self.arch_tag,
             ]
-        if self.proxy_token:
-            proxy_env = (
-                "http_proxy=http://{username}:{password}"
-                "@{host}:{port}".format(
-                    username=self.build_cookie,
-                    password=self.proxy_token,
-                    host=self.proxy_host,
-                    port=self.proxy_port))
-            args.insert(0, proxy_env)
+        env = dict(os.environ)
+        if self.proxy_url:
+            proxy_env = 'http_proxy={}'.format(self.proxy_url)
+            env['http_proxy'] = proxy_env
         if self.branch is not None:
             args.extend(["--branch", self.branch])
         if self.git_repository is not None:
@@ -72,7 +64,7 @@ class SnapBuildManager(DebianBuildManager):
         if self.git_path is not None:
             args.extend(["--git-path", self.git_path])
         args.append(self.name)
-        self.runSubProcess(self.build_snap_path, args)
+        self.runSubProcess(self.build_snap_path, args, env=env)
 
     def iterate_BUILD_SNAP(self, retcode):
         """Finished building the snap."""
