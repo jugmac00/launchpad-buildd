daf2625343899ffe449efd6cd06d5a7af929c0bb
Colin Watson <cjwatson@canonical.com>
2016-12-05 13:03:34 +0000

    Improve handling of attaching resources after initial deployment.

--- charm/reactive/launchpad-buildd.py
+++ charm/reactive/launchpad-buildd.py
@@ -43,44 +43,49 @@ def install_packages():
     host.mkdir(cache_dir, perms=0o755)
     to_install = []
     packages = ["launchpad-buildd", "python-lpbuildd"]
+    options = ["--option=Dpkg::Options::=--force-confold"]
     resource_paths = [hookenv.resource_get(package) for package in packages]
     if all(path and os.path.getsize(path) for path in resource_paths):
         # Install from resources.
+        changed = False
         for package, resource_path in zip(packages, resource_paths):
             local_path = os.path.join(cache_dir, "{}.deb".format(package))
+            to_install.append((local_path, resource_path))
             if host.file_hash(local_path) != host.file_hash(resource_path):
-                to_install.append((local_path, resource_path))
+                changed = True
+        if not changed:
+            return
+        options.append("--reinstall")
     else:
         # We don't have resource-provided packages, so just install from the
         # PPA.
         to_install.extend([(None, package) for package in packages])
-    if to_install:
-        new_paths = [new_path for _, new_path in to_install]
-        try:
-            status_set(None, "Installing {}".format(",".join(packages)))
-            fetch.apt_unhold(packages)
-            fetch.apt_install(new_paths)
-            fetch.apt_hold(packages)
-        except subprocess.CalledProcessError:
-            status_set(
-                "blocked",
-                "Unable to install packages {}".format(",".join(packages)))
-        else:
-            for local_path, resource_path in to_install:
-                if local_path is not None:
-                    shutil.copy2(resource_path, local_path)
-            # ntp.buildd isn't likely to work outside of the Canonical
-            # datacentre, and LXD containers can't set the system time.
-            # Let's just not worry about NTP.
-            config_path = "/etc/launchpad-buildd/default"
-            with open(config_path) as config_file:
-                config = config_file.read()
-            config = re.sub(r"^ntphost = .*", "ntphost = ", config, flags=re.M)
-            with open(config_path + ".new", "w") as new_config_file:
-                new_config_file.write(config)
-            os.rename(config_path + ".new", config_path)
-            remove_state("launchpad-buildd.needs_install")
-            set_state("launchpad-buildd.installed")
+    new_paths = [new_path for _, new_path in to_install]
+    try:
+        status_set(None, "Installing {}".format(",".join(packages)))
+        fetch.apt_unhold(packages)
+        fetch.apt_install(new_paths, options=options)
+        fetch.apt_hold(packages)
+    except subprocess.CalledProcessError:
+        status_set(
+            "blocked",
+            "Unable to install packages {}".format(",".join(packages)))
+    else:
+        for local_path, resource_path in to_install:
+            if local_path is not None:
+                shutil.copy2(resource_path, local_path)
+        # ntp.buildd isn't likely to work outside of the Canonical
+        # datacentre, and LXD containers can't set the system time.  Let's
+        # just not worry about NTP.
+        config_path = "/etc/launchpad-buildd/default"
+        with open(config_path) as config_file:
+            config = config_file.read()
+        config = re.sub(r"^ntphost = .*", "ntphost = ", config, flags=re.M)
+        with open(config_path + ".new", "w") as new_config_file:
+            new_config_file.write(config)
+        os.rename(config_path + ".new", config_path)
+        remove_state("launchpad-buildd.needs_install")
+        set_state("launchpad-buildd.installed")
 
 
 @when("apt.installed.bzr-builder")
