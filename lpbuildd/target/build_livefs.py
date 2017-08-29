4cfb1214a6a400e6c8b9ad131b0569d1adb0f4b0
Colin Watson <cjwatson@canonical.com>
2017-08-29 09:53:24 +0100

    Only install snapd, lxd, and squashfuse if they're available.

--- lpbuildd/target/build_livefs.py
+++ lpbuildd/target/build_livefs.py
@@ -82,7 +82,9 @@ class BuildLiveFS(Operation):
     def install(self):
         deps = ["livecd-rootfs"]
         if self.args.backend == "lxd":
-            deps.extend(["snapd", "fuse", "squashfuse"])
+            for dep in "snapd", "fuse", "squashfuse":
+                if self.backend.is_package_available(dep):
+                    deps.append(dep)
         self.backend.run(["apt-get", "-y", "install"] + deps)
         if self.args.arch == "i386":
             self.backend.run([
