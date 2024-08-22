bacb37225e9cb9d987a9320a3e7c105ddc42fe0a
ruinedyourlife <quentin.debhi@canonical.com>
2024-08-22 14:42:27 +0200

    fix: install snapcraft as beta

--- lpbuildd/target/tests/test_build_source.py
+++ lpbuildd/target/tests/test_build_source.py
@@ -131,7 +131,7 @@ class TestBuildSource(TestCase):
                     RanAptGet(
                         "install", "bzr"
                     ),
-                    RanSnap("install", "--classic", "sourcecraft"),
+                    RanSnap("install", "--classic", "--beta", "sourcecraft"),
                     RanCommand(["mkdir", "-p", "/home/buildd"]),
                 ]
             ),
@@ -157,7 +157,7 @@ class TestBuildSource(TestCase):
                     RanAptGet(
                         "install", "git"
                     ),
-                    RanSnap("install", "--classic", "sourcecraft"),
+                    RanSnap("install", "--classic", "--beta", "sourcecraft"),
                     RanCommand(["mkdir", "-p", "/home/buildd"]),
                 ]
             ),
@@ -211,7 +211,7 @@ class TestBuildSource(TestCase):
                     RanCommand(
                         ["snap", "set", "core", "proxy.store=store-id"]
                     ),
-                    RanSnap("install", "--classic", "sourcecraft"),
+                    RanSnap("install", "--classic", "--beta", "sourcecraft"),
                     RanCommand(["mkdir", "-p", "/home/buildd"]),
                 ]
             ),
@@ -248,7 +248,7 @@ class TestBuildSource(TestCase):
                         "socat",
                         "git",
                     ),
-                    RanSnap("install", "--classic", "sourcecraft"),
+                    RanSnap("install", "--classic", "--beta", "sourcecraft"),
                     RanCommand(["mkdir", "-p", "/home/buildd"]),
                 ]
             ),
