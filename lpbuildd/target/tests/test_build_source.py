4f2beddff10279696f267c12c8d5f0d23680e557
RuinedYourLife <debhi.quentin@gmail.com>
2024-08-29 06:46:26 +0000

    Add destructive mode for sourcecraft command

--- lpbuildd/target/tests/test_build_source.py
+++ lpbuildd/target/tests/test_build_source.py
@@ -530,7 +530,7 @@ class TestBuildSource(TestCase):
             MatchesListwise(
                 [
                     RanBuildCommand(
-                        ["sourcecraft", "pack", "-v"],
+                        ["sourcecraft", "pack", "-v", "--destructive-mode"],
                         cwd="/home/buildd/test-image/.",
                     ),
                 ]
@@ -558,7 +558,7 @@ class TestBuildSource(TestCase):
             MatchesListwise(
                 [
                     RanBuildCommand(
-                        ["sourcecraft", "pack", "-v"],
+                        ["sourcecraft", "pack", "-v", "--destructive-mode"],
                         cwd="/home/buildd/test-image/build-aux/",
                     ),
                 ]
@@ -591,7 +591,7 @@ class TestBuildSource(TestCase):
             MatchesListwise(
                 [
                     RanBuildCommand(
-                        ["sourcecraft", "pack", "-v"],
+                        ["sourcecraft", "pack", "-v", "--destructive-mode"],
                         cwd="/home/buildd/test-image/.",
                         **env,
                     ),
@@ -630,7 +630,7 @@ class TestBuildSource(TestCase):
                 ),
                 AnyMatch(
                     RanBuildCommand(
-                        ["sourcecraft", "pack", "-v"],
+                        ["sourcecraft", "pack", "-v", "--destructive-mode"],
                         cwd="/home/buildd/test-image/.",
                     )
                 ),
